b6733335221b2040c6038181cdaeddd902358042
Iksned <Iksned@gmail.com>
2017-07-20 18:45:26 +0400

    Delete test2.java

--- test2.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Iks on 19.07.2017.
- */
-public class test2 {
-    public static void main(String[] args) {
-        System.out.println("hey");
-        new Main_Window();
-    }
-}
