79aea65ed97a9b01f566472b1dab9cabae710e6e
Iksned <Iksned@gmail.com>
2017-08-21 11:40:20 +0400

    Delete Observable.java

--- src/main/java/concontrol/Observable.java
+++
@@ -1,8 +0,0 @@
-package concontrol;
-
-public interface Observable {
-
-    public void notifyObserver();
-    public void registerObserver(CatalogObserver obs);
-    public void removeObsever(CatalogObserver obs);
-}
