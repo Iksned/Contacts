71ec3e7af11e0e4f74b68aad415b2cd01c802184
Iksned <Iksned@gmail.com>
2017-08-28 14:38:10 +0400

    Delete Constants.java

--- src/main/java/DAO/Constants.java
+++
@@ -1,9 +0,0 @@
-package DAO;
-
-
-import java.io.File;
-
-public class Constants {
-    public final static File saveFile = new File("src/main/resources/catal.xml");
-    public final static File checkFile = new File("src/main/resources/XSDcheck.xsd");
-}
