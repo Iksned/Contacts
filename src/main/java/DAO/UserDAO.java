fe97dcf46e894963014cc83c7f2b7e8295e764d1
Iksned <Iksned@gmail.com>
2017-08-28 14:43:07 +0400

    Add files via upload

---
+++ src/main/java/DAO/UserDAO.java
@@ -0,0 +1,22 @@
+package DAO;
+
+import ConModel.User;
+
+import java.util.List;
+
+public interface UserDAO extends CatalogDAO<User>{
+    @Override
+    void create(User ob);
+
+    @Override
+    User read(String id);
+
+    @Override
+    List<User> readAll(String ob);
+
+    @Override
+    void update(User newOb);
+
+    @Override
+    void delete(User ob);
+}
